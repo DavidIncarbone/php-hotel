e4dc1b3a0ea99d97d731dba67b8d1f29f3d5852b
DavidIncarbone <david.incarbone@gmail.com>
2025-03-12 17:13:15 +0100

    Print hotel info and linked BS

--- index.php
+++ index.php
@@ -4,12 +4,72 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>PHP Hotel</title>
 </head>
 
 <body>
 
-    <h1>PHP Hotel</h1>
+    <h1 class="text-center">PHP Hotel</h1>
+
+    <?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+    // var_dump($hotels);
+
+    foreach ($hotels as $hotel) {
+        // var_dump($hotel);
+
+        foreach ($hotel as $key => $value) {
+            echo "$key : $value <br>";
+        };
+        echo "<br> <hr>";
+    }
+
+
+
+    ?>
+
+
 
 </body>
 
